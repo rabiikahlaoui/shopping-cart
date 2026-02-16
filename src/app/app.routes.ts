776ff7e4d7fb3ffeb4e35a8205a467aca04b2606
Rabii Kahlaoui <rabii.kahlaoui.ext@allianz-trade.com>
2026-02-16 16:15:07 +0100

    feat: init app layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { CartPage } from './domains/cart/components/cart-page/cart-page';
+import { ProductsPage } from './domains/products/components/products-page/products-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', pathMatch: 'full', redirectTo: 'products' },
+    { path: 'products', component: ProductsPage },
+    { path: 'cart', component: CartPage }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './core/layout/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+  imports: [RouterOutlet, Navbar],
+  templateUrl: './app.html'
 })
 export class App {
-  protected readonly title = signal('shopping-cart');
 }
