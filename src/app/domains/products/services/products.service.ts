f2c3744984b16815f7f24083a32770c014c14d05
Rabii Kahlaoui <rabii.kahlaoui.ext@allianz-trade.com>
2026-02-16 17:30:06 +0100

    chore: add code linter

--- src/app/domains/products/services/products.service.ts
+++ src/app/domains/products/services/products.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { BaseApiService } from '../../../core/api/base-api.service';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
@@ -9,10 +9,7 @@ import { Product } from '../model/product.model';
 })
 export class ProductsService extends BaseApiService {
   protected override readonly apiPath = '/api/products';
-
-  constructor(private readonly http: HttpClient) {
-    super();
-  }
+  private readonly http: HttpClient = inject(HttpClient);
 
   getProducts(): Observable<Product[]> {
     return this.http.get<Product[]>(this.apiUrl);
